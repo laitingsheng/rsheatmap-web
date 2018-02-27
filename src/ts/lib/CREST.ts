d54b431b10486453c66bc97da19b0de46299a5b3
Tinson Lai <laitinson@gmail.com>
2018-02-27 19:33:11 +1100

    finalise CREST, circular & rectangular

--- src/ts/lib/CREST.ts
+++ src/ts/lib/CREST.ts
@@ -1,4 +1,4 @@
-import { Comparable, ComparableDataObject } from './Util';
+import { ComparableDataObject } from './Util';
 import Circle = google.maps.Circle;
 import LatLng = google.maps.LatLng;
 import Rectangle = google.maps.Rectangle;
@@ -6,36 +6,6 @@ import Rectangle = google.maps.Rectangle;
 const computeDistanceBetween = google.maps.geometry.spherical.computeDistanceBetween;
 const computeHeading = google.maps.geometry.spherical.computeHeading;
 const computeOffset = google.maps.geometry.spherical.computeOffset;
-
-export abstract class AbstractTick<R extends Comparable<R>>
-    extends ComparableDataObject<AbstractTick<R>> {
-    // auto convert
-    [Symbol.toPrimitive](hint: string) {
-        if(hint === 'number')
-            return this.tick;
-
-        if(hint === 'string')
-            return this.toString();
-
-        return true;
-    }
-
-    compareTo(o: AbstractTick<R>): number {
-        if(this === o)
-            return 0;
-
-        let re = this.tick - o.tick;
-        if(re)
-            return re;
-        if(re = this.region.compareTo(o.region))
-            return re;
-        return this.status - o.status;
-    }
-
-    protected constructor(readonly tick: number, readonly region: R, readonly status: number) {
-        super();
-    }
-}
 
 export class RectRegion extends ComparableDataObject<RectRegion> {
     static fromRectangle(rectangle: Rectangle): RectRegion {
@@ -98,11 +68,11 @@ export function lineSweepCRESTRect(regions: Array<RectRegion>): number | never {
         } else
             canvas.set(v.maxX, [new Set([v.minY, v.maxY]), new Map()]);
     });
+    const xAxis = Array.from(canvas.entries());
+    xAxis.sort((l, r) => l[0] - r[0]);
 
     // line-sweep
     let sweep = new Map<number, number>(), prevA: [number, Axes] = null;
-    const xAxis = Array.from(canvas.entries());
-    xAxis.sort((l, r) => l[0] - r[0]);
     xAxis.forEach(a => {
         const ySweep = Array.from(sweep.entries());
         ySweep.sort((l, r) => l[0] - r[0]);
@@ -141,8 +111,19 @@ export function lineSweepCRESTRect(regions: Array<RectRegion>): number | never {
 }
 
 export class CirRegion extends ComparableDataObject<CirRegion> {
-    readonly centre: LatLng;
-    readonly radius: number;
+    readonly minX: number;
+    readonly midX: number;
+    readonly maxX: number;
+    readonly minY: number;
+    readonly maxY: number;
+
+    get centre(): LatLng {
+        return this.circle.getCenter();
+    }
+
+    get radius(): number {
+        return this.circle.getRadius();
+    }
 
     static fromCircle(circle: Circle): CirRegion {
         return new CirRegion(circle);
@@ -175,71 +156,84 @@ export class CirRegion extends ComparableDataObject<CirRegion> {
 
     private constructor(readonly circle: Circle) {
         super();
+        const bound = circle.getBounds(), sw = bound.getSouthWest(), ne = bound.getNorthEast();
+        this.minX = sw.lng();
+        this.midX = this.centre.lng();
+        this.maxX = ne.lng();
+        this.minY = sw.lat();
+        this.maxY = ne.lat();
     }
 }
 
 export function lineSweepCRESTCir(regions: Array<CirRegion>): number | never {
     let maxOverlap = 1;
 
-    // add all critical events
-    const canvas = new Map<number, Axes>();
+    // pre-process and identify x ticks of critical events
+    const canvas = new Map<number, Map<number, number>>();
     regions.forEach((v, i) => {
-        const bound = v.circle.getBounds(), sw = bound.getSouthWest(),
-            ne = bound.getNorthEast(), xMin = sw.lng(), xMid = v.centre.lng(), xMax = ne.lng(),
-            yMin = sw.lat(), yMax = ne.lat();
-
-        if(!canvas.get(xMin))
-            canvas.set(xMin, [new Set(), new Map()]);
-
-        let t = canvas.get(xMid);
-        if(t) {
-            let yt = t[1].get(yMin);
-            if(yt)
-                t[1].set(yMin, yt + 1);
-            else
-                t[1].set(yMin, 1);
-
-            yt = t[1].get(yMax);
-            if(yt)
-                t[1].set(yMax, yt - 1);
-            else
-                t[1].set(yMax, -1);
-        } else
-            canvas.set(xMid, [new Set(), new Map([[yMin, 1], [yMax, -1]])]);
-
-        if(!canvas.get(xMax))
-            canvas.set(xMax, [new Set(), new Map()]);
+        if(!canvas.get(v.minX))
+            canvas.set(v.minX, new Map());
+        if(!canvas.get(v.midX))
+            canvas.set(v.midX, new Map());
+        if(!canvas.get(v.maxX))
+            canvas.set(v.maxX, new Map());
 
         for(let j = i + 1; j < regions.length; ++j) {
             const child = regions[j];
             if(v.intersect(child)) {
-                const ins = v.intersectWith(child);
-                ticks.push(...events);
-                v.events.set(events[0].toString(), events[0]);
-                v.events.set(events[1].toString(), events[1]);
-                child.events.set(events[0].toString(), events[0]);
-                child.events.set(events[1].toString(), events[1]);
+                const [i1, i2] = v.intersectWith(child);
+                if(!canvas.get(i1.lng()))
+                    canvas.set(i1.lng(), new Map());
+                if(!canvas.get(i2.lng()))
+                    canvas.set(i2.lng(), new Map());
             }
         }
     });
-    ticks.sort(compareFunction);
+    const xAxis = Array.from(canvas.entries());
+    xAxis.sort((l, r) => l[0] - r[0]);
 
-    // line sweep
-    let sweep: Array<number> = [], prevTick: CirTick;
-    ticks.forEach(xTick => {
+    // process the critical events
+    regions.forEach(r => {
+        for(const t of xAxis) {
+            const bound = r.circle.getBounds();
 
-        switch(xTick.status) {
-            case 0:
+            if(bound.getNorthEast().lng() <= t[0])
                 break;
-            case 1:
-                break;
-            case 2:
-                break;
-            default:
-                throw `corrupted data`;
+
+            if(bound.getSouthWest().lng() >= t[0])
+                continue;
+
+            const tmp = new LatLng(r.centre.lat(), t[0]),
+                tmpDist = computeDistanceBetween(tmp, r.centre),
+                height = Math.sqrt(r.radius * r.radius - tmpDist * tmpDist),
+                u = computeOffset(tmp, height, 0), l = computeOffset(tmp, height, -180);
+
+            let yt = t[1].get(l.lat());
+            if(yt)
+                t[1].set(l.lat(), yt + 1);
+            else
+                t[1].set(l.lat(), 1);
+
+            yt = t[1].get(u.lat());
+            if(yt)
+                t[1].set(u.lat(), yt - 1);
+            else
+                t[1].set(u.lat(), -1);
         }
-        prevTick = xTick;
     });
 
+    // line-sweep
+    xAxis.forEach(a => {
+        const sweep = Array.from(a[1].entries());
+        sweep.sort((l, r) => l[0] - r[0]);
+        let overlap = 0;
+        sweep.forEach(v => {
+            overlap += v[1];
+            if(overlap > maxOverlap)
+                maxOverlap = overlap;
+        });
+    });
+
+    alert(maxOverlap);
     return maxOverlap;
 }
